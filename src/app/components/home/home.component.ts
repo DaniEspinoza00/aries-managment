b7cf2cf226df91c5ffc39dedb20cb1bb3d7a2a86
DaniEspinoza00 <jorgedanielespinoza00@gmail.com>
2024-08-30 00:06:52 -0300

    Home page finished

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,13 +1,15 @@
 import { NgOptimizedImage } from '@angular/common';
 import { Component } from '@angular/core';
+import {MatButtonModule} from '@angular/material/button';
+import {MatCardModule} from '@angular/material/card';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [NgOptimizedImage],
+  imports: [NgOptimizedImage,MatButtonModule,MatCardModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
-export class HomeComponent {
+export class HomeComponent{
 
 }
